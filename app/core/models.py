7f97202b3aa51752caf3a36408133481bab2599d
Smdonline <4cVVgJN5dZYUhtM>
2024-08-11 23:12:11 +0200

    Added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,29 @@
-# from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Mamager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the System"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
